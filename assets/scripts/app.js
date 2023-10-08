d3779d885675626c48b0245f2effeab16a77e22a
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-08 12:08:49 +0300

    added a Component base class and practiced inheritance

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,9 +12,37 @@ class DOMHelper {
   }
 }
 
-class Tooltip {
+class Component {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElement = document.getElementById(hostElementId);
+    } else {
+      this.hostElement = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
+
+  detach() {
+    if (this.element) {
+      this.element.remove();
+      // this.element.parentElement.removeChild(this.element);
+    } // extra sanity check you could say
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+}
+
+class Tooltip extends Component {
   constructor(closeNotifierFunction) {
+    // super('active-projects', true);
+    super();
     this.closeNotifier = closeNotifierFunction;
+    this.create();
   }
 
   closeTooltip = () => {
@@ -22,18 +50,12 @@ class Tooltip {
     this.closeNotifier();
   };
 
-  detach() {
-    this.element.remove();
-    // this.element.parentElement.removeChild(this.element);
-  }
-
-  attach() {
+  create() {
     const tooltipElement = document.createElement('div');
     tooltipElement.className = 'card';
     tooltipElement.textContent = 'DUMMY!';
     tooltipElement.addEventListener('click', this.closeTooltip);
     this.element = tooltipElement;
-    document.body.append(tooltipElement);
   }
 }
 
